ab67f1343bffcd016f60cf58f972f7d61a052f3f
Abdyy <82438230+AbdyyEee@users.noreply.github.com>
2023-11-08 23:44:23 +0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from setuptools import setup, find_packages
 
 with open("README.md", "r") as f:
-    description = f.read()
+    long_description = f.read()
+
 setup(
     name="PylibMS",
-    version="0.1.2",
+    version="0.1.4",
     author="AbdyyEee",
     packages=find_packages(),
     license="mit",
     description="Library for Nintendo's LMS file formats (MSBT, MSBP) for Nintendo 3DS and Wii U",
-    long_description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AbdyyEee/PylibMS",
     keywords=["nintendo", "msbf", "msbt", "msbp", "modding", "lms", "libmessagestudio"],
     python_requires=">=3.10"
 )
