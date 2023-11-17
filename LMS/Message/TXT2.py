36c27cfcfef1d01cc008f8c6551201914c010536
AbdyyEee <Abdyyy123456788@gmail.com>
2023-11-17 14:08:31 +0400

    Fix the get_tag_info function and format file

--- LMS/Message/TXT2.py
+++ LMS/Message/TXT2.py
@@ -18,50 +18,58 @@ class TXT2:
         self.block: LMS_Block = LMS_Block()
         self.messages: list[str] = []
         self.preset: list[dict] = base_preset
-    
+
     def tag_encoded(self, tag: str) -> bool:
         """Returns if a tag is encoded or not.
-        
+
         :param `tag`: The tag to check."""
         return "." in tag[: tag.find(":")]
-    
+
     def get_decoded_tag_information(self, tag: str) -> dict:
         """Returns a dictionary of all the information on a tag if its decoded.
-        
-        :param `tag`: The tag to get the infomation for."""
+
+        :param `tag`: The tag to get
+        the infomation for."""
         result = {}
         if not self.tag_encoded(tag):
-            group_name = tag[1:tag.find(":")]
-            tag_name = tag[tag.find(":") + 1: tag.rfind(":")]
+            group_name = tag[1 : tag.find(":")]
+            tag_name = tag[tag.find(":") + 1 : tag.rfind(":")]
 
             result["group_name"] = group_name
             result["tag_name"] = tag_name
-            dictionary_parameters = dict(re.findall(r'(\w+)\s*=\s*["\']([^"\']+)["\']', tag))
+            dictionary_parameters = dict(
+                re.findall(r'(\w+)\s*=\s*["\']([^"\']+)["\']', tag)
+            )
 
             result["parameters"] = dictionary_parameters
             return result
 
     def get_tag_info(self, tag: str) -> dict:
-        """Returns a dictionary of all the information on a tag."""
+        """Returns a dictionary of all the information on a tag assuming its decoded.
+        
+        :param `tag`: The tag to get the information for."""
         result = {}
-        is_encoded = "." in tag[: tag.find(":")]
 
-        if not is_encoded:
-            group_name = tag[1:tag.find(":")]
-            tag_name = tag[tag.find(":") + 1: tag.rfind(":")]
+        if self.tag_encoded(tag):
+            return
 
-            result["group_name"] = group_name
-            result["tag_name"] = tag_name
-            result["parameters"] = {}
+        group_name = tag[1 : tag.find(":")]
+        tag_name = tag[tag.find(":") + 1 : tag.rfind(":")]
 
-            return result
+        result["group_name"] = group_name
+        result["tag_name"] = tag_name
+        dictionary_parameters = dict(
+            re.findall(r'(\w+)\s*=\s*["\']([^"\']+)["\']', tag)
+        )
 
+        result["parameters"] = dictionary_parameters
+        return result
 
     def split_message_by_tag(self, message: str) -> list[str]:
         """Splits a message by the control tags.
 
         :param `message`: The message to split."""
-        return re.split(r'(<[^>]+>)', message)
+        return re.split(r"(<[^>]+>)", message)
 
     def generate_preset_msbp(self, name: str, project: MSBP) -> dict:
         """Generates an editable preset .py file for use with tag decoding mode of `preset`.
@@ -82,8 +90,8 @@ class TXT2:
                 for tag_index in group["tag_indexes"]:
                     # Prevent the system tags from being written as it is defined in base_preset
                     if group_index == 0 and tag_index in [0, 1, 2, 3, 4]:
-                        continue 
-                    
+                        continue
+
                     tag = project.TAG2.tags[tag_index]
                     tag_name = tag["name"]
                     read_function_name = f"read_{group_name}_{tag_name}"
